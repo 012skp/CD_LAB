2e745d3ed3a349b3fa937ae08683ae30ee1e9549
Shailesh Kumar <012skp@gmail.com>
2017-08-17 08:41:38 +0530

    updating

---
+++ a.c
@@ -0,0 +1,17 @@
+int func(){
+	return 3;
+}
+
+#include<stdio.h>
+
+int main(){
+	int f();
+	int abc = 0xAA35B;
+	int _ = 34e3F;
+	float aa = 34.4e-3;
+	int z = abc++;
+	char str[] = "sdaflsf\vfdaf";
+	int aaaa = 1<< 24;
+	printf("%s\n",str);
+	return 0;
+}
--- lexical.cc
+++ lexical.cc
@@ -1,9 +1,11 @@
 #include<lexical.h>
+#define pb push_back
+
 extern int TOKEN_LEN;
 
 
 	lexical_analyser::lexical_analyser(char *file_name){
-		delimeter = (char*)"+-/*%^&|~ \n\t=;,(){}[]\"\'.!<>";
+		delimeter = (char*)"+-/*%^&|~ \n\t=;:,(){}[]\"\'.!<>";
 		memset(filename,0,20);
 		if(!file_name){
 			printf("file_name is NULL\n");
@@ -17,6 +19,55 @@ extern int TOKEN_LEN;
 		}
 		// Set previous_token as non void-space.
 		previous_token = '0';
+		// Initialise keywords.
+		keywords.pb((char*)("auto"));
+		keywords.pb((char*)("double"));
+		keywords.pb((char*)("int"));
+		keywords.pb((char*)("struct"));
+		keywords.pb((char*)("break"));
+		keywords.pb((char*)("else"));
+		keywords.pb((char*)("switch"));
+		keywords.pb((char*)("long"));
+		keywords.pb((char*)("case"));
+		keywords.pb((char*)("enum"));
+		keywords.pb((char*)("register"));	
+		keywords.pb((char*)("typedef"));
+		keywords.pb((char*)("char"));
+		keywords.pb((char*)("return"));
+		keywords.pb((char*)("extern"));
+		keywords.pb((char*)("union"));
+		keywords.pb((char*)("const"));
+		keywords.pb((char*)("float"));
+		keywords.pb((char*)("short"));
+		keywords.pb((char*)("unsigned"));
+		keywords.pb((char*)("continue"));
+		keywords.pb((char*)("for"));
+		keywords.pb((char*)("signed"));
+		keywords.pb((char*)("void"));
+		keywords.pb((char*)("default"));
+		keywords.pb((char*)("goto"));
+		keywords.pb((char*)("sizeof"));
+		keywords.pb((char*)("volatile"));
+		keywords.pb((char*)("do"));
+		keywords.pb((char*)("if"));
+		keywords.pb((char*)("static"));
+		keywords.pb((char*)("while"));
+		
+	}
+	
+
+	// Validates the token is a valid token.
+	char* lexical_analyser::validate(char *token){
+		if(is_delimeter(token[0])) return token;
+		// Else it is either keyword, or identifier, or string literal, or numeric constant.
+		if(is_keyword(token)) return token;
+		else if(is_identifier(token)) return token;
+		else if(is_string_literal(token)) return token;
+		else if(is_numeric_constant(token)) return token;
+		else{
+			printf("Invalid token %s\n",token);
+			return NULL;
+		}
 	}
 
 	char* lexical_analyser::get_next_token(char *token){
@@ -26,13 +77,14 @@ extern int TOKEN_LEN;
 		if(next_token(token) == NULL) return NULL;
 		
 
-		// If " is incountered..???
+		// If " is incountered...
+		// Handle string constansts...
 		if(token[0] == '\"'){
-			if(till_quote(token)) return token;
+			if(till_quote(token)) return validate(token);
 			else return NULL;
 		}
 
-		// If comment with "//"  or "/*" are incountered pass whole line as token
+		// If comment with "//"  or "/*" are incountered ignore till comments end.
 		if(token[0] == '/'){		
 			char ch;
 			if(!feof(fp)) 
@@ -49,30 +101,33 @@ extern int TOKEN_LEN;
 			// else do nothing..
 
 		}
-		/* If previous_token was non void-space
-		   then register this token in previous_token
-		   and send the token.
-		*/
-		if(previous_token == '0'){
+		
+		// No validation required.
+		// Compiler will validate.
+		if(token[0] == '#') return token;
 
-			// If this token is void-space store it.
+
+		//Ignore multiple spaces like ' ', '\n', '\t'
+		// If previous token was not space, return this token whatever it is.
+		if(previous_token == '0'){
+	
+			// If this token is space store it.
 			if(token[0] == ' ' || token[0] == '\t' || token[0] == '\n') 
 				previous_token = token[0]; 
 			else
 				previous_token = '0';
 			return token;
 		}
+		// If previous token was space.
 		else{
-			// If previous_token was void-space and this is also 
-			// void-space then don't pass delimeter.
+			// If this token is also space then don't return this token.
 			if(token[0] == ' ' || token[0] == '\t' || token[0] == '\n'){
-				// this token is void-space don't pass it.
 				goto start;
 			}
 			else{
-				// this token is non void-space pass it.
+				// this token is not space pass it.
 				previous_token = '0';
-				return token;
+				return validate(token);
 			}
 			
 		}
@@ -198,6 +253,149 @@ extern int TOKEN_LEN;
 
 	char* lexical_analyser::get_delimeter(){
 		return delimeter;
+	}
+
+	char* lexical_analyser::is_keyword(char *token){
+		for(int i=0;i<keywords.size();i++) 
+			if(strcmp(token,keywords[i]) == 0) 
+				return token;
+		return NULL;
+	}
+
+	char * lexical_analyser::is_identifier(char *token){
+		// first char should be '_' or '{a-z}' or '{A-Z}'.
+		char fc = token[0];
+		if(fc == '_' or (fc>=65 and fc<=90) or (fc<=122 and fc >= 97) );
+		else return NULL;
+		
+		// Rest of char now may contain '{0-9}'.
+		for(int i=1;i<strlen(token);i++){
+			fc = token[i];
+			if(fc == '_' or  (fc<= 57 and fc>= 48) or (fc>=65 and fc<=90) or (fc<=122 and fc >= 97) );
+			else return NULL;
+		}
+
+		return token;
+		
+	}
+
+	char* lexical_analyser::is_string_literal(char *token){
+		// First char must be '"'
+		char fc = token[0];
+		if(fc == '\"') return token;
+		else return NULL;
+	}
+
+	char* lexical_analyser::is_numeric_constant(char *token){
+		int idx = 0;
+		if(token[0] == '-' || token[0] == '+') idx++;
+		if(token[idx] == '.') goto FLOAT;
+		if(strncmp(token+idx,"0x",2) == 0) goto HEX;
+		else if(strncmp(token+idx,"0.",2) == 0) { idx+=1; goto FLOAT;}
+		else if(token[idx] == '0') goto OCTAL;
+		else goto DECIMAL;
+
+
+DECIMAL:	// DECIMAL Starts.	
+		
+		// This may contain float.
+		int i;
+		for(i=idx;i<strlen(token);i++){
+			if(token[i] <= 57 and token[i] >= 48);
+			else break;	
+		}
+		if(i == strlen(token)) return token;
+		else if(token[i] == '.'){ idx = i; goto FLOAT;}
+		else if(token[i] == 'E' or token[i] == 'e'){ idx = i; goto EXPONENT;}
+		else if(token[i] == 'u' or token[i] == 'U' or token[i] == 'l' or token[i] == 'L'){
+			idx = i;
+			goto INT_SUFFIX;
+		}
+		else return NULL;
+		// DECIMAL Ends.
+
+HEX:		// HEX Starts.
+
+		for(i=idx+2;i<strlen(token);i++){
+			// Either 0-9 or a-f or A-F.
+			if((token[i] <= 57 and token[i] >= 48) or (token[i]<=70 and token[i]>= 65) or (token[i]<=102 and token[i]>= 97) );
+			else break;
+		}
+		if(i == strlen(token)) return token;
+		else if(token[i] == 'u' or token[i] == 'U' or token[i] == 'l' or token[i] == 'L'){
+			idx = i;
+			goto INT_SUFFIX;
+		}
+		else return NULL;
+		// HEX Ends.
+
+OCTAL:		// OCTAL Starts.
+		for(i=idx+1;i<strlen(token);i++){
+			// Either 0-9 or a-f or A-F.
+			if(token[i] <= 55 and token[i] >= 48 );
+			else break;
+		}
+		if(i == strlen(token)) return token;
+		else if(token[i] == 'E' or token[i] == 'e'){idx = i; goto EXPONENT;}
+		else if(token[i] == 'u' or token[i] == 'U' or token[i] == 'l' or token[i] == 'L'){
+			idx = i;
+			goto INT_SUFFIX;
+		}
+		else return NULL;
+		// OCTAL Ends.
+
+FLOAT:		// FLOAT Starts.
+	
+		for(i=idx+1;i<strlen(token);i++){
+			if(token[i] <= 57 and token[i] >= 48);
+			else break;	
+		}
+		if(i == strlen(token)) return token;
+		else if(token[i] == 'E' or token[i] == 'e'){
+			idx = i;
+			goto EXPONENT;		
+		}
+
+
+FLOAT_SUFFIX:	// FLOAT_SUFFIX Starts.
+		// Pointer(idx) at the start of suffix.
+		
+		i  = idx;
+		if(token[i] == 'f' or token[i] == 'F' or token[i] == 'l' or token[i] == 'L'){
+			// Float suffix is either f|F or l|L not both. So this must be end of token.
+			if(i==strlen(token)-1) return token;
+			else return NULL;
+		}
+		else return NULL;
+		// FLOAT_SUFFIX Ends.
+
+
+INT_SUFFIX:	// INT_SUFFIX Starts.
+		// Pointer(idx) at the start of suffix.
+		// TODO Write for it.
+
+		return token;
+
+		// INT_SUFFIX Ends.
+
+
+EXPONENT:	// EXPONENT Starts. Exponent make a constant to float.
+		// Pointer(idx) at pos 'e' or 'E'
+		idx++;
+		
+		char fc = token[idx];
+		if(fc == '+' || fc == '-') idx++;
+		for(i=idx;i<strlen(token);i++){
+			
+			if(token[i] <= 57 and token[i] >= 48);
+			else break;
+		}
+		if(i==strlen(token)) return token;
+		else if(token[i] == 'f' or token[i] == 'F' or token[i] == 'l' or token[i] == 'L'){
+			idx = i;
+			goto FLOAT_SUFFIX;
+		}
+		else return NULL;
 	}
 
 
