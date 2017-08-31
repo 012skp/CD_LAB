cafbc6fbea213ed2909e80e172b09b8e19b0a425
CD LAB <012skp@gmail.com>
2017-08-31 09:19:52 +0530

    ........

---
+++ lexical_analyser/a.c
@@ -0,0 +1,22 @@
+int func(){
+	return 3;
+}
+
+#include<stdio.h>
+
+int main(){
+	int f();
+	int abc = 0xAA350uLABC;
+	int _ = 34e3F;
+	int hex = 0xBULL;
+	int heee = 0343434378;
+	float aa = 34.4e-3f;
+	int z = abc++;
+	int ffff = .454;
+	int fff = 35.4E-3lL;
+	int zz = 232ere;
+	char str[] = "sdaflsf\vfdaf";
+	int aaaa = 1<< 24;
+	printf("%s\n",str);
+	return 0;
+}
